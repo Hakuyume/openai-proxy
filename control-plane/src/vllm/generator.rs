ba3011dc78764015a0e9564528910e5956bf8270
Toru Ogawa <Hakuyume@users.noreply.github.com>
2026-01-30 08:33:39 +0900

    templates

--- control-plane/src/vllm/generator.rs
+++ control-plane/src/vllm/generator.rs
@@ -1,4 +1,3 @@
-use std::time::Duration;
 use tonic_envoy::envoy::config::core::v3 as core_v3;
 use tonic_envoy::envoy::config::route::v3 as route_v3;
 use tonic_envoy::envoy::r#type::matcher::v3 as matcher_v3;
@@ -7,8 +6,7 @@ pub(super) struct Generator<'a> {
     pub(super) models: &'a schemas::List<schemas::Model>,
     pub(super) route_config_name: &'a String,
     pub(super) cluster_name: &'a String,
-    pub(super) timeout: Option<Duration>,
-    pub(super) idle_timeout: Option<Duration>,
+    pub(super) template_route: Option<&'a route_v3::Route>,
 }
 
 impl Generator<'_> {
@@ -33,65 +31,55 @@ impl Generator<'_> {
 
     fn route_list_models(&self) -> anyhow::Result<route_v3::Route> {
         let body = serde_json::to_string(self.models)?;
-        Ok(route_v3::Route {
-            r#match: Some(route_v3::RouteMatch {
-                path_specifier: Some(route_v3::route_match::PathSpecifier::Path(
-                    "/v1/models".to_owned(),
-                )),
-                headers: vec![route_v3::HeaderMatcher {
-                    name: ":method".to_owned(),
-                    header_match_specifier: Some(
-                        route_v3::header_matcher::HeaderMatchSpecifier::StringMatch(
-                            matcher_v3::StringMatcher {
-                                match_pattern: Some(
-                                    matcher_v3::string_matcher::MatchPattern::Exact(
-                                        "GET".to_owned(),
-                                    ),
-                                ),
-                                ..matcher_v3::StringMatcher::default()
-                            },
-                        ),
-                    ),
-                    ..route_v3::HeaderMatcher::default()
-                }],
-                ..route_v3::RouteMatch::default()
-            }),
-            response_headers_to_add: vec![core_v3::HeaderValueOption {
+
+        let mut route = self.template_route.cloned().unwrap_or_default();
+        let match_ = route.r#match.get_or_insert_default();
+        match_.path_specifier = Some(route_v3::route_match::PathSpecifier::Path(
+            "/v1/models".to_owned(),
+        ));
+        match_.headers.push(route_v3::HeaderMatcher {
+            name: ":method".to_owned(),
+            header_match_specifier: Some(
+                route_v3::header_matcher::HeaderMatchSpecifier::StringMatch(
+                    matcher_v3::StringMatcher {
+                        match_pattern: Some(matcher_v3::string_matcher::MatchPattern::Exact(
+                            "GET".to_owned(),
+                        )),
+                        ..matcher_v3::StringMatcher::default()
+                    },
+                ),
+            ),
+            ..route_v3::HeaderMatcher::default()
+        });
+        route
+            .request_headers_to_add
+            .push(core_v3::HeaderValueOption {
                 header: Some(core_v3::HeaderValue {
                     key: "content-type".to_owned(),
                     value: "application/json".to_owned(),
                     ..core_v3::HeaderValue::default()
                 }),
                 ..core_v3::HeaderValueOption::default()
-            }],
-            action: Some(route_v3::route::Action::DirectResponse(
-                route_v3::DirectResponseAction {
-                    status: http::StatusCode::OK.as_u16() as _,
-                    body: Some(core_v3::DataSource {
-                        specifier: Some(core_v3::data_source::Specifier::InlineString(body)),
-                        ..core_v3::DataSource::default()
-                    }),
-                },
-            )),
-            ..route_v3::Route::default()
-        })
+            });
+        let action = misc::get_or_insert_default!(
+            &mut route.action,
+            route_v3::route::Action::DirectResponse
+        );
+        action.status = http::StatusCode::OK.as_u16() as _;
+        action.body.get_or_insert_default().specifier =
+            Some(core_v3::data_source::Specifier::InlineString(body));
+        Ok(route)
     }
 
     fn route_fallback(&self) -> anyhow::Result<route_v3::Route> {
-        Ok(route_v3::Route {
-            r#match: Some(route_v3::RouteMatch {
-                path_specifier: Some(route_v3::route_match::PathSpecifier::Prefix("/".to_owned())),
-                ..route_v3::RouteMatch::default()
-            }),
-            action: Some(route_v3::route::Action::Route(route_v3::RouteAction {
-                cluster_specifier: Some(route_v3::route_action::ClusterSpecifier::Cluster(
-                    self.cluster_name.clone(),
-                )),
-                timeout: self.timeout.map(TryInto::try_into).transpose()?,
-                idle_timeout: self.idle_timeout.map(TryInto::try_into).transpose()?,
-                ..route_v3::RouteAction::default()
-            })),
-            ..route_v3::Route::default()
-        })
+        let mut route = self.template_route.cloned().unwrap_or_default();
+        let match_ = route.r#match.get_or_insert_default();
+        match_.path_specifier = Some(route_v3::route_match::PathSpecifier::Prefix("/".to_owned()));
+        let action =
+            misc::get_or_insert_default!(&mut route.action, route_v3::route::Action::Route);
+        action.cluster_specifier = Some(route_v3::route_action::ClusterSpecifier::Cluster(
+            self.cluster_name.clone(),
+        ));
+        Ok(route)
     }
 }
